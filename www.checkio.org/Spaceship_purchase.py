86c0295a1bbf767625fe68764cdbb37d69e3b14d
kugua <kuguakugua@gmail.com>
2012-11-12 18:29:38 +0800

    rename

--- www.checkio.org/Spaceship_purchase.py
+++ www.checkio.org/Spaceship_purchase.py
@@ -1,3 +1,9 @@
+#Write a program that, depending on the Sofia¡¯s and old man¡¯s starting bargaining sums, the step on which they will increase or decrease the price during the argument, will calculate on which price they will agree.
+
+#Input data: contains four integer numbers: the initial Sofia's offer, Sofia's raise to his offer, the initial fare required by the old man, and the old man's reduction of his fare;
+#Output data: the amount of money that Sofia will pay for the spaceship.
+
+
 def checkio(offers):
     """
        the amount of money that Petr will pay for the ride
